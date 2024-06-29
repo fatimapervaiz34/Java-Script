5c220b254f7d780c7b5046551335c4db7d03d8b3
fatimapervaiz34 <fatimahpervaiz95@gmail.com>
2024-06-29 20:53:57 +0500

    first

---
+++ script.js
@@ -0,0 +1,12 @@
+var email=prompt("Enter your email")
+var password=prompt("enter your password")
+
+if (email===" kashan"){
+console.log( "you are kashan");
+}
+else if(password==="kashan"){
+    alert("you are admin");
+}
+else{
+    console.log("condition false")
+}
